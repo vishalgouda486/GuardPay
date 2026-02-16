87c8775740e69413aa2acc0bb5e504b95a09e7ec
vishalgouda486 <vishalgouda486@gmail.com>
2026-02-16 15:58:20 +0530

    polished admin page 6

--- Backend/main.py
+++ Backend/main.py
@@ -121,7 +121,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://guard-pay-red.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
